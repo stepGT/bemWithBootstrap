9fb4ad645ff14b6f0f264728fb8f85a2d9351ea8
Andrey <stepgt@mail.ru>
2020-01-27 15:23:21 +1000

    Added scroll to top. Tablet complete. Change JQuery

--- js/main.js
+++ js/main.js
@@ -19,6 +19,60 @@ $(document).ready(function () {
     slidesToScroll: 3,
     arrows: false,
     dots: true,
-    dotsClass: 'dots-style'
+    dotsClass: 'dots-style',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+
+  /**
+   *
+   */
+  $('ul.menu a[href^="#"]').click(function () {
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 500);
+    $('ul.menu a[href^="#"]').css({'color': '#212121'});
+    $(this).css({'color': '#004bee'});
+    return false;
+  });
+
+  $('.menu-icon').click(function () {
+    $('nav').slideToggle(500);
+    $('ul').css({
+      'display': 'flex',
+      'flex-direction': 'column'
+    });
+    if ($('.menu-icon').html() === '<i class="fas fa-bars"></i>') {
+      $(this).html('<i class="fas fa-times"></i>');
+    }
+    else {
+      $(this).html('<i class="fas fa-bars"></i>');
+    }
+  });
+
+  /**
+   *
+   */
+  $(window).scroll(function () {
+    var toTop = $('#toTop');
+    if ($(this).scrollTop() !== 0) {
+      toTop.fadeIn();
+    }
+    else {
+      toTop.fadeOut();
+    }
+    toTop.click(function () {
+      $('body, html').animate({
+        scrollTop: 0
+      }, 800);
+    });
   });
 });
